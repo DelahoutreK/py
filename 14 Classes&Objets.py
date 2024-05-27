01ad5ecc8d943fbb79d2fcbf77163eacc88148db
DelahoutreK <kenandelahoutre@gmail.com>
2024-05-28 01:34:46 +0200

    3

--- 14 Classes&Objets.py
+++ 14 Classes&Objets.py
@@ -5,3 +5,11 @@ class Couleur(): # une classe
     x = "rouge"
 p1 = Couleur() # l'obet instancié depuis la classe
 print(p1.x)
+
+# typiquement on utilisera une fonction __init__ pour déclarer une classe
+class Eleves:
+    def __init__(self, nom, age):
+        self.nom = nom
+        self.age = age
+eleve1 = Eleves("Paul",30)
+print(eleve1.nom + str(eleve1.age))
