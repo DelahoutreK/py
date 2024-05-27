0718045b56cd035a25bc60e4dbac485e7e209e9a
Kenan Delahoutre <kenandelahoutre@gmail.com>
2024-05-27 22:15:02 +0200

    update synch

---
+++ 14 Classes&Objets.py
@@ -0,0 +1,7 @@
+# une classe est un constructeur d'objet, un recette
+# un objet est l'instance d'une classe, un produit de la recette
+
+class Couleur(): # une classe
+    x = "rouge"
+p1 = Couleur() # l'obet instancié depuis la classe
+print(p1.x)
